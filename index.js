d85c1f0e124d3a87c0aaeafb7fe022802cc8be0c
Maddy <mmcintosh2k14@gmail.com>
2019-12-02 16:01:34 -0500

    imports express and sets up api to listen on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+// imports express 
+const express = require('express');
+
+const server = express();
+
+// creates a function to have the api return a message once it is successfully listening on the specified port 
+const port = 4000;
+server.listen(port, () => console.log(`API listening on port ${port}!`));
+
+// client makes a GET request to /api/users to receive a list of users
+// if error retrieving the users, cancel request and respond with status code 500
+// return JSON object: {error: 'The users information could not be retrived.'}
